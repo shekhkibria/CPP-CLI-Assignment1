0801c1422b3c8d712f04e2d5c35625265a022e2c
Shekh Md. Kibria <shekh.kibria@enosisbd.com>
2020-02-11 19:14:51 +0600

    Implement QuickSort

--- SortingAlgorithm/SortingAlgorithm/SortingAlgorithm.h
+++ SortingAlgorithm/SortingAlgorithm/SortingAlgorithm.h
@@ -5,12 +5,24 @@
 using namespace System;
 
 namespace SortingAlgorithm {
-
-	public ref class MergeSort
+	public ref class Sort
+	{
+	public:
+		virtual void sort(int *, int)=0;
+	};
+	public ref class MergeSort: public Sort
 	{
 	public:
 		MergeSort();
 		~MergeSort();
-		void sort(int *, int);
+		void sort(int *, int) override;
+	};
+
+	public ref class QuickSort: public Sort
+	{
+	public:
+		QuickSort();
+		~QuickSort();
+		void sort(int *, int) override;
 	};
 }
