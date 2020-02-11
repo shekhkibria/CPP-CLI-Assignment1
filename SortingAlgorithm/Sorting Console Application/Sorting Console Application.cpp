ad7986ba424af22c44dac4698029310ae5fcf9ad
Shekh Md. Kibria <shekh.kibria@enosisbd.com>
2020-02-11 18:27:06 +0600

    Add sorting console application project and implemented merge sort feature

---
+++ SortingAlgorithm/Sorting Console Application/Sorting Console Application.cpp
@@ -0,0 +1,23 @@
+// Sorting Console Application.cpp : main project file.
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace System;
+using namespace SortingAlgorithm;
+int main()
+{
+    Console::WriteLine(L"Hello World");
+	int n;
+	//std::cin.get();
+
+	int arr[] = { 4,5,3,2,6,7,4,5,9,8,5,3 };
+	MergeSort^ msort=gcnew MergeSort();
+	msort->sort(arr,12-1);
+	for (int i = 0; i < 12; i++)
+	{
+		std::cout << arr[i] << std::endl;
+	}
+	std::cin.get();
+    return 0;
+}
