d01463f4996eba91753dc786f9bb3b1922b86614
Shekh Md. Kibria <shekh.kibria@enosisbd.com>
2020-02-11 13:40:04 +0600

    Create C++/CLR Library project named SortingAlgorithm

---
+++ SortingAlgorithm/SortingAlgorithm/SortingAlgorithm.h
@@ -0,0 +1,13 @@
+// SortingAlgorithm.h
+
+#pragma once
+
+using namespace System;
+
+namespace SortingAlgorithm {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
